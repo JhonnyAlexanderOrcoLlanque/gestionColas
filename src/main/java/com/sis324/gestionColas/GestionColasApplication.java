7989b30c361b05f1319a5c78b15fa0737a30a46c
JhonnyAlexanderOrcoLlanque <jhonnyorco4@gmail.com>
2024-06-20 01:55:53 -0400

    first commit

---
+++ src/main/java/com/sis324/gestionColas/GestionColasApplication.java
@@ -0,0 +1,13 @@
+package com.sis324.gestionColas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionColasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionColasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sis324/gestionColas/GestionColasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sis324.gestionColas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionColasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
